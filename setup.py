3385ae1bc8127ca7be61ce1a6d55efb0b3e921e3
Luis Nell <ln@nells.eu>
2013-07-27 14:26:02 +0200

    simplify Extension() setup.

--- setup.py
+++ setup.py
@@ -24,19 +24,19 @@ def sources():
                 cpp_files.append(os.path.join(dirpath, filename))
     return cpp_files
 
-ext = {}
-ext['sources'] = sources()
+kwargs = {}
+kwargs['sources'] = sources()
 
 if sys.platform == 'win32':
     java_home = os.getenv('JAVA_HOME')
     if not java_home:
         print 'Environment Variable JAVA_HOME must be set.'
         sys.exit(-1)
-    ext['libraries'] = ['Advapi32']
-    ext['library_dir'] = [os.path.join(java_home, 'lib')]
-    ext['define_macros'] = [('WIN32', 1)]
-    ext['extra_compile_args'] = ['/EHsc']
-    ext['include_dirs'] = [
+    kwargs['libraries'] = ['Advapi32']
+    kwargs['library_dir'] = [os.path.join(java_home, 'lib')]
+    kwargs['define_macros'] = [('WIN32', 1)]
+    kwargs['extra_compile_args'] = ['/EHsc']
+    kwargs['include_dirs'] = [
         'src/native/common/include',
         'src/native/python/include',
         os.path.join(java_home, 'include'),
@@ -59,10 +59,10 @@ elif sys.platform == 'darwin':
     elif osx in ('10.7', '10.8'):
         java_home = ('/System/Library/Frameworks/JavaVM.framework/'
                      'Versions/Current/')
-    ext['libraries'] = ['dl']
-    ext['library_dir'] = [os.path.join(java_home, 'Libraries')]
-    ext['define_macros'] = [('MACOSX', 1)]
-    ext['include_dirs'] = [
+    kwargs['libraries'] = ['dl']
+    kwargs['library_dir'] = [os.path.join(java_home, 'Libraries')]
+    kwargs['define_macros'] = [('MACOSX', 1)]
+    kwargs['include_dirs'] = [
         'src/native/common/include',
         'src/native/python/include',
         os.path.join(java_home, 'Headers'),
@@ -95,9 +95,9 @@ else:
                 "pull request on github: https://github.com/originell/jpype/"
                 % '\n'.join(possible_homes))
 
-    ext['libraries'] = ['dl']
-    ext['library_dir'] = [os.path.join(java_home, 'lib')]
-    ext['include_dirs'] = [
+    kwargs['libraries'] = ['dl']
+    kwargs['library_dir'] = [os.path.join(java_home, 'lib')]
+    kwargs['include_dirs'] = [
         'src/native/common/include',
         'src/native/python/include',
         os.path.join(java_home, 'include'),
@@ -106,14 +106,7 @@ else:
         os.path.join(java_home, '..', 'include', 'linux'),
     ]
 
-jpypeLib = Extension(name='_jpype',
-                     sources=ext.get('sources'),
-                     libraries=ext.get('libraries'),
-                     define_macros=ext.get('define_macros'),
-                     include_dirs=ext.get('include_dirs'),
-                     library_dirs=ext.get('library_dir'),
-                     extra_compile_args=ext.get('extra_compile_args'),
-                    )
+jpypeLib = Extension(name='_jpype', **kwargs)
 
 setup(
     name='JPype1',
