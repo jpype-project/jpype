8918249f72cb16f31a27980728d76d679fc9532c
baztian <bastian.dev@gmail.com>
2014-03-08 18:56:14 +0100

    Default to rtd documentation theme locally (vs. on Read The Docs).
    Run pip install -r dev-requirements.txt

--- doc/conf.py
+++ doc/conf.py
@@ -108,6 +108,16 @@ html_theme = 'default'
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
