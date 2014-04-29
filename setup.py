3d3b8c7d6e5d69541d5007a3d9d30474041c2920
marscher <m.scherer@fu-berlin.de>
2014-04-29 20:22:48 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ class my_build_ext(build_ext):
 
 setup(
     name='JPype1',
-    version='0.5.5.1',
+    version='0.5.5.2',
     description='A Python to Java bridge.',
     long_description=(read_utf8('README.rst') + '\n\n' +
                       read_utf8('doc/CHANGELOG.rst') + '\n\n' +
