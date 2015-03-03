jpype-project/jpype

commit c2f1ecb51149228f1fc195c2a7855c24eff97cb0

some global references were cleaned with local reference cleaners and vice versa

src/native/common/jp_class.cpp
@@ -618,7 +618,7 @@ bool JPClass::isSubclass(JPClass* o)
 	JPCleaner cleaner;
 
 	jclass jo = o->getClass();
-	cleaner.addLocal(jo);
+	cleaner.addGlobal(jo);
 
 	if (JPEnv::getJava()->IsAssignableFrom(m_Class, jo))
 	{

src/native/common/jp_field.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-   Copyright 2004 Steve Ménard
+   Copyright 2004 Steve Mï¿½nard
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -79,7 +79,7 @@ HostRef* JPField::getStaticAttribute()
 	JPType* type = JPTypeManager::getType(m_Type);
 	JPCleaner cleaner;
 	jclass claz = m_Class->getClass();
-	cleaner.addLocal(claz);
+	cleaner.addGlobal(claz);
 	
 
 	return type->getStaticValue(claz, m_FieldID, m_Type);
@@ -107,7 +107,7 @@ void JPField::setStaticAttribute(HostRef* val)
 		
 	JPCleaner cleaner;
 	jclass claz = m_Class->getClass();
-	cleaner.addLocal(claz);
+	cleaner.addGlobal(claz);
 
 	type->setStaticValue(claz, m_FieldID, val);		
 	TRACE_OUT;

src/native/common/jp_methodoverload.cpp
@@ -205,7 +205,7 @@ HostRef* JPMethodOverload::invokeStatic(vector<HostRef*>& arg)
 	}
 	
 	jclass claz = m_Class->getClass();
-	cleaner.addLocal(claz);
+	cleaner.addGlobal(claz);
 
 	JPType* retType = JPTypeManager::getType(m_ReturnType);
 
@@ -233,11 +233,14 @@ HostRef* JPMethodOverload::invokeInstance(vector<HostRef*>& args)
 			HostRef* obj = args[i];
 	
 			JPType* type = JPTypeManager::getType(m_Arguments[i]);
-			v[i-1] = type->convertToJava(obj);		
-			if (type->isObjectType())
-			{
-				cleaner.addLocal(v[i-1].l);
-			}
+			v[i-1] = type->convertToJava(obj);
+			// below seems not to be necessary, since jvalues are clean by jpmalloccleaner
+//			if (type->isObjectType())
+//			{
+//			//TODO : investigate: this is not valid for either local or global ref
+//				// add hostref to garbage collection
+//				cleaner.add(v[i-1].l);
+//			}
 		}
 		
 		JPType* retType = JPTypeManager::getType(m_ReturnType);
@@ -246,7 +249,7 @@ HostRef* JPMethodOverload::invokeInstance(vector<HostRef*>& args)
 		cleaner.addLocal(c);
 	
 		jclass clazz = m_Class->getClass();
-		cleaner.addLocal(clazz);
+		cleaner.addGlobal(clazz);
 	
 		res = retType->invoke(c, clazz, m_MethodID, v.borrow());
 		TRACE1("Call finished");

src/native/common/jp_objecttypes.cpp
@@ -116,9 +116,9 @@ void JPObjectType::setInstanceValue(jobject c, jfieldID fid, HostRef* obj)
 jarray JPObjectType::newArrayInstance(int sz)
 {
 	JPCleaner cleaner;
-	
+
 	jclass c = getClass();
-	cleaner.addLocal(c);
+	cleaner.addGlobal(c);
 	
 	return JPEnv::getJava()->NewObjectArray(sz, c, NULL);
 }

src/native/common/jp_primitivetypes.cpp
@@ -23,7 +23,7 @@ jobject JPPrimitiveType::convertToJavaObject(HostRef* obj)
 	JPClass* c = JPTypeManager::findClass(tname);
 
 	jclass jc = c->getClass();
-	cleaner.addLocal(jc);
+	cleaner.addGlobal(jc);
 
 	vector<HostRef*> args(1);
 	args[0] = obj;

src/native/python/include/py_hostenv.h
@@ -99,7 +99,7 @@ public :
 	static void deleteObjectJValueDestructor(void* data, void* desc)
 	{
 		jvalue* pv = (jvalue*)data;
-		JPEnv::getJava()->DeleteGlobalRef(pv->l);
+		JPEnv::getJava()->DeleteLocalRef(pv->l);
 		delete pv;
 	}
 

src/native/python/jpype_python.cpp
@@ -106,7 +106,7 @@ PyObject* JPypeJavaProxy::createProxy(PyObject*, PyObject* arg)
 
 		PyObject* self;
 		PyObject* intf;
-
+		//TODO: why is self not initialized?
 		JPyArg::parseTuple(arg, "OO", &self, &intf);
 
 		std::vector<jclass> interfaces;
@@ -120,7 +120,7 @@ PyObject* JPypeJavaProxy::createProxy(PyObject*, PyObject* arg)
 			PyObject* claz = JPyObject::getAttrString(subObj, "__javaclass__");
 			PyJPClass* c = (PyJPClass*)claz;
 			jclass jc = c->m_Class->getClass();
-			cleaner.addLocal(jc);
+			cleaner.addGlobal(jc);
 			interfaces.push_back(jc);
 		}
 		

commit 301c90cb50d7f3c1d97fdc5791ef74f24cd0d0d4

Implement unit test for #121: Possible (java heap) memory leak caused by commit c2f1ecb5

test/jpypetest/attr.py
@@ -191,3 +191,16 @@ class AttributeTestCase(common.JPypeTestCase):
         c = self.__jp.TestOverloadC()
         self.assertEqual(c.foo(1), "foo(int) in C: 1")
         self.assertEqual(c.foo(), "foo() in A")
+
+    def testPassedObjectGetsCleanedUp(self):
+        h = self.__jp.Test1()
+        block_size = 1024 * 1024 * 10
+        def allocate_then_free():
+            byte_buffer = jpype.JClass('java.nio.ByteBuffer')
+            inst = byte_buffer.allocate(1024 * 1024 * 100)
+             # passing the object back to java seems to stop it being collected
+            result = h.callWithSomething(inst)
+        rt = jpype.java.lang.Runtime.getRuntime()
+        free = rt.freeMemory()
+        for x in range(0, 10 * free / block_size):
+            allocate_then_free()

test/jpypetest/common.py
@@ -37,7 +37,7 @@ class JPypeTestCase(unittest.TestCase) :
             classpath_arg %= path.join(root, 'classes')
             jpype.startJVM(jvm_path, "-ea",
                            # "-Xcheck:jni",
-                           "-Xmx256M", "-Xms64M", classpath_arg)
+                           "-Xmx256M", "-Xms16M", classpath_arg)
         self.jpype = jpype.JPackage('jpype')
         if sys.version < '3':
             self.assertCountEqual = self.assertItemsEqual