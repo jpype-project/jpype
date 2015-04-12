ca2b7f25f8f303d40125956b114a55f3ed71ed55
marscher <m.scherer@fu-berlin.de>
2015-04-13 00:20:05 +0200

    [appveyor] build without numpy

--- appveyor/deploy_pypi.ps1
+++ appveyor/deploy_pypi.ps1
@@ -1,8 +1,15 @@
 function deploy() {
     pip install twine wheel
     
+    # with numpy
     python setup.py bdist_wheel
     
+    # now again without numpy
+    rm -r build
+    python setup.py bdist_wheel --disable-numpy
+    
+    # upload to pypi
+    ls dist
     twine upload -u $env:PYPI_USER -p $env:PYPI_PASS dist/*
 }
 
