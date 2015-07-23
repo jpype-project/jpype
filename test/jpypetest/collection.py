e9359c470048fb7d58f1f95c59b6e111ba07adeb
baztian <bastian.dev@gmail.com>
2015-07-23 08:24:49 +0200

    Make collection test more explicit.

--- test/jpypetest/collection.py
+++ test/jpypetest/collection.py
@@ -10,4 +10,4 @@ class CollectionTestCase(common.JPypeTestCase):
         collection = jpype.java.util.ArrayList()
         collection.add(1)
         collection.add(2)
-        self.assertEqual([1, 2], list(collection))
+        self.assertEqual([1, 2], [i for i in collection])
