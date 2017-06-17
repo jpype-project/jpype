d06ef9d928d178ff6d8677dd122a5f7d936c197b
Nelson, Karl E <nelson85@llnl.gov>
2017-06-17 10:07:43 -0700

    Still trying to get prerequisites.

--- appveyor/install.sh
+++ appveyor/install.sh
@@ -10,27 +10,39 @@ echo PYTHON=$PYTHON
 SETUP=/setup-$ARCH
 if [ $PYTHON = "python3" ]; then
 	PIP=pip3
+	EASYINSTALL=easy_install-3.6
 else
 	PIP=pip
+	EASYINSTALL=easy_install-2.7
 fi
 
 # Install prereqs
+echo "==== update gcc"
 $SETUP -q -P gcc-core,gcc-g++
+echo "==== update python"
 $SETUP -q -P $PYTHON,$PYTHON-numpy,$PYTHON-devel,$PYTHON,$PYTHON-setuptools,$PYTHON-nose
-$PIP install mock
+echo "==== get modules"
+$EASYINSTALL pip
+$EASYINSTALL mock
+#$PIP install mock
 
 # Check versions
+echo "==== Check versions"
 "$ANT_HOME"/bin/ant -version
 $PYTHON --version
 
+echo "==== Check modules"
 $PYTHON -c 'import pip; print(sorted(["%s==%s" % (i.key, i.version) for i in pip.get_installed_distributions()]))'
 
 # Get the arch size
+echo "==== Check arch"
 $PYTHON -c "import struct; print(struct.calcsize('P') * 8)"
 
 # Build the test harness
+echo "==== Build test"
 "$ANT_HOME"/bin/ant -f test/build.xml
 
 # Install the package
+echo "==== Build module"
 $PYTHON setup.py install
 
