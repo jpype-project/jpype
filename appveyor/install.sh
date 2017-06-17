72ff67fc4ca1999fd3d0fc618dfa68b37a1bb394
Nelson, Karl E <nelson85@llnl.gov>
2017-06-17 00:48:09 -0700

    Still need to find nose

--- appveyor/install.sh
+++ appveyor/install.sh
@@ -8,13 +8,14 @@ echo ANT_HOME=$ANT_HOME
 echo ANT_BIN=$ANT_BIN
 echo PATH=$PATH
 echo PYTHON=$PYTHON
-echo PIP=$PIP
 
 # Install prereqs
 if [ $PYTHON = "python3" ]; then
 	/setup-$ARCH -q -P python3,python3-numpy,python3-devel,python3,python3-setuptools,python3-nose
+	PIP=pip3
 else
 	/setup-$ARCH -q -P python,python-numpy,python-devel,python,python-setuptools,python-nose
+	PIP=pip
 fi
 
 # Check versions
--- appveyor/runTestsuite.sh
+++ appveyor/runTestsuite.sh
@@ -1,5 +1,12 @@
 export PATH="/bin:/usr/bin"
-nosetests --with-xunit --all-modules -w test/jpypetest
+
+if [ $PYTHON = "python3" ]; then
+	NOSETESTS=nosetests-3.6
+else
+	NOSETESTS=nosetests-2.7
+fi
+
+$NOSETESTS --with-xunit --all-modules -w test/jpypetest
 status=$?
 echo "result code of nosetests:" $status 
 exit $status
