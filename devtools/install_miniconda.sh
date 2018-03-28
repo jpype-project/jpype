cd18339c505e42e5532e0a3d48d4fcb5a8984a21
Martin K. Scherer <m.scherer@fu-berlin.de>
2018-03-28 17:47:50 +0200

    added dev conda recipe with build variants

---
+++ devtools/install_miniconda.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# make TARGET overrideable with env
+: ${TARGET:=$HOME/miniconda}
+
+function install_miniconda {
+	if [ -d $TARGET ]; then echo "file exists"; return; fi
+	echo "installing miniconda to $TARGET"
+        if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then                                 
+             platform="Linux"
+        elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+             platform="MacOSX"
+        fi     
+	wget http://repo.continuum.io/miniconda/Miniconda3-latest-$platform-x86_64.sh -O mc.sh -o /dev/null
+	bash mc.sh -b -f -p $TARGET
+}
+
+install_miniconda
+export PATH=$TARGET/bin:$PATH
+hash -r
+conda config add channels conda-forge
+conda install conda-build -c defaults
