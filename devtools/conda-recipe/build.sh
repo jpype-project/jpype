45a1ed0d093f1ace8d9a0f322a647c7304f441ea
Martin K. Scherer <m.scherer@fu-berlin.de>
2018-03-28 20:12:27 +0200

    add build.sh

---
+++ devtools/conda-recipe/build.sh
@@ -0,0 +1,10 @@
+which g++
+
+export CCACHE_BASEDIR=${SRC_DIR}
+export CCACHE_NOHASHDIR="true"
+export CCACHE_DIR=${HOME}/.ccache
+
+python setup.py install --single-version-externally-managed --record record.txt
+
+ccache -s
+
