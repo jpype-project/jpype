60f5223c073b9aeb23b4f46d410976afe3f59658
Martin K. Scherer <m.scherer@fu-berlin.de>
2018-03-28 18:48:48 +0200

    f

--- devtools/install_miniconda.sh
+++ devtools/install_miniconda.sh
@@ -20,7 +20,7 @@ export PATH=$TARGET/bin:$PATH
 hash -r
 conda config --set always_yes True
 conda config --set quiet True
-conda config add channels conda-forge
+conda config --add channels conda-forge
 
 conda install conda-build -c defaults
 
