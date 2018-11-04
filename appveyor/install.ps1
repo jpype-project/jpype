1c4273f12949826fa789ad39cc553946fcb97d23
Martin K. Scherer <m.scherer@fu-berlin.de>
2018-11-04 14:09:15 +0100

    install with pip

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -7,9 +7,8 @@ python.exe -c "import struct; print(struct.calcsize('P') * 8)"
 # compiled extensions and are not provided as pre-built wheel packages,
 # pip will build them from source using the MSVC compiler matching the
 # target Python version and architecture
-conda.exe update --all -y 
-conda.exe install nose setuptools -y
-pip.exe install -r "test-requirements.txt" # -r dev-requirements.txt
+pip.exe install nose setuptools -r test-requirements.txt
+#pip.exe install -r "test-requirements.txt" # -r dev-requirements.txt
 
 ant.exe -f test\\build.xml
 
