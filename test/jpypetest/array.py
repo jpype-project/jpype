5d66b63265ca81ec64fad786dabf68f85c1becda
Nelson, Karl E <nelson85@llnl.gov>
2018-11-04 08:11:56 -0800

    Pull tests for numpy array bug from master.

--- test/jpypetest/array.py
+++ test/jpypetest/array.py
@@ -296,6 +296,55 @@ class ArrayTestCase(common.JPypeTestCase):
         jarr[:] = a
         self.assertCountEqual(a, jarr)
 
+    @unittest.skipUnless(haveNumpy(), "numpy not available")
+    def testInitFromNPIntArray(self):
+        import numpy as np
+        n = 100
+        a = np.random.random(n).astype(np.int)
+        jarr = jpype.JArray(jpype.JInt)(a)
+        self.assertCountEqual(a, jarr)
+
+    @unittest.skipUnless(haveNumpy(), "numpy not available")
+    def testInitFromNPDoubleArray(self):
+        import numpy as np
+        n = 100
+        a = np.random.random(n).astype(np.float)
+        jarr = jpype.JArray(jpype.JDouble)(a)
+        self.assertCountEqual(a, jarr)
+
+    @unittest.skipUnless(haveNumpy(), "numpy not available")
+    def testInitFromNPDoubleArrayFloat32(self):
+        import numpy as np
+        n = 100
+        a = np.random.random(n).astype(np.float32)
+        jarr = jpype.JArray(jpype.JDouble)(a)
+        self.assertCountEqual(a, jarr)
+
+    @unittest.skipUnless(haveNumpy(), "numpy not available")
+    def testInitFromNPDoubleArrayFloat64(self):
+        import numpy as np
+        n = 100
+        a = np.random.random(n).astype(np.float64)
+        jarr = jpype.JArray(jpype.JDouble)(a)
+        self.assertCountEqual(a, jarr)
+
+    @unittest.skipUnless(haveNumpy(), "numpy not available")
+    def testInitFromNPFloatArrayInt(self):
+        import numpy as np
+        a = np.array([1,2,3],dtype=np.int32)
+        jarr = jpype.JArray(jpype.JFloat)(a)
+        print(a)
+        print(jarr)
+        self.assertCountEqual(a, jarr)
+
+    @unittest.skipUnless(haveNumpy(), "numpy not available")
+    def testSetFromNPFloatArrayInt(self):
+        import numpy as np
+        a = np.array([1,2,3],np.int32)
+        jarr = jpype.JArray(jpype.JFloat)(len(a))
+        jarr[:] = a
+        self.assertCountEqual(a, jarr)
+
     def testArrayCtor1(self):
         jobject = jpype.JClass('java.lang.Object')
         jarray = jpype.JArray(jobject)
