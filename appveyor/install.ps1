013e67a39894cc965ebddf339c40235e82e64f09
KOLANICH <KOLANICH@users.noreply.github.com>
2018-11-16 18:25:36 +0000

    Fixing backslashes.

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -14,4 +14,4 @@ ant.exe -f test\\build.xml
 
 # Build the compiled extension and run the project tests
 python.exe setup.py bdist_wheel
-pip install --upgrade .\dist\*.whl
+pip install --upgrade \\dist\\*.whl
