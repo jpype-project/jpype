1ce61e37202c3bdb917ee4313bf10fd05b1c2711
KOLANICH <KOLANICH@users.noreply.github.com>
2018-11-17 14:18:38 +0000

    trying to fix

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -9,14 +9,21 @@ python.exe -c "import struct; print(struct.calcsize('P') * 8)"
 # compiled extensions and are not provided as pre-built wheel packages,
 # pip will build them from source using the MSVC compiler matching the
 # target Python version and architecture
-pip.exe install --upgrade setuptools
 
 #seem like pip is broken in 3.4 and is not available via python -m
+git clone --depth=1 https://github.com/pypa/setuptools.git
+python setuptools\bootstrap.py
+python setuptools\setup.py install
+
+git clone --depth=1 git+https://github.com/pypa/wheel.git
+python wheel\setup.py install
+
 git clone --depth=1 https://github.com/pypa/pip.git
 python pip\setup.py install
 Remove-Item .\pip -Force -Recurse
+Remove-Item .\setuptools -Force -Recurse
 
-pip install --upgrade git+https://github.com/pypa/setuptools_scm.git git+https://github.com/pypa/wheel.git
+pip install --upgrade git+https://github.com/pypa/setuptools_scm.git
 pip install --upgrade nose -r test-requirements.txt
 #pip.exe install -r "test-requirements.txt" # -r dev-requirements.txt
 
