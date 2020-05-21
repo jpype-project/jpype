a1d50409fb5ff45014433f5bb12899a716038219
Karl Nelson <nelson85@llnl.gov>
2020-05-21 06:56:54 -0700

    Work on testing

--- testDoc.py
+++ testDoc.py
@@ -1,16 +1,23 @@
 import jpype
+import sys
 from jpype.types import *
 import jpype.imports
 jpype.startJVM(classpath='project/jpype_java/dist/*')
 
 from java.nio.file import Paths, Files
 
+html = JClass("org.jpype.html.Html")
+hw = JClass("org.jpype.html.HtmlWriter")
 jdz = JClass("org.jpype.html.JavadocZip")(Paths.get("project/jpype_java/jdk-8u251-docs-all.zip"))
 jde = JClass("org.jpype.html.JavadocExtractor")
 #jip = jdz.getPath(JClass("java.lang.Double"))
 #lines = Files.readAllLines(jip)
 # for l in lines:
 #    print(l)
+# for k,v in html.ENTITIES.items():
+#    print(k, html.decode("&"+str(k)+";").charAt(0))
+#print(html.decode("public static&trade;double&nbsp;sum(double&nbsp;a,double&nbsp;b)"))
+# sys.exit(0)
 
 jis = jdz.getInputStream(JClass("java.lang.Double"))
 jd = jde.extractStream(jis)
@@ -34,7 +41,7 @@ print("=========================================================")
 
 print("Methods:")
 for c in jd.methods:
-    print(c)
+    print(hw.asString(jd.processNode(c)))
     print("---------------------------------------------------------")
 
 print("=========================================================")
