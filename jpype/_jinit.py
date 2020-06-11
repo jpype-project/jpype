ec54973a829b6769bd4aedbdb404f70de2bc914c
Karl Nelson <nelson85@llnl.gov>
2020-06-10 18:16:06 -0700

    Documentation

--- jpype/_jinit.py
+++ jpype/_jinit.py
@@ -22,7 +22,20 @@ JInitializers = []
 
 
 def onJVMStart(func):
-    """Register a function to be called after JVM is started"""
+    """Register a function to be called after JVM is started
+
+    This can be used to load module resources that depend on the JVM such as loading
+    classes.  If the JVM is not started, the user supplied function is held in a list until
+    the JVM starts.  When startJVM is called, all functions on the deferred list are called
+    and the list is cleared.  If the JVM is already started, then the function is called
+    immediately.
+
+    Errors from the function will either be raised immediately if the JVM is started, or 
+    from startJVM if the JVM is not yet started.
+
+    Args:
+       func (callable): a function to call when the JVM is started.
+    """
     registerJVMInitializer(func)
 
 
