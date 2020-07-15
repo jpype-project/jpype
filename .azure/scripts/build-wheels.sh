a18293dafe62ee828d2e97850edc623c7bc01a0c
Karl Nelson <nelson85@llnl.gov>
2020-07-14 20:52:08 -0700

    Attempt to fix release

--- .azure/scripts/build-wheels.sh
+++ .azure/scripts/build-wheels.sh
@@ -10,18 +10,23 @@ pys=(${pys[@]//*27*/})
 
 # Compile wheels
 for PYBIN in "${pys[@]}"; do
+    echo "Compile $PYBIN"
     ls -l /io/dist
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/dist/$package_name-*.tar.gz -w wheelhouse/
 done
+echo "=============="
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/$package_name-*.whl; do
+    echo "Audit $whl"
     auditwheel repair --plat $PLAT "$whl" -w /io/wheelhouse/
 done
+echo "=============="
 
 # Install packages and test
 for PYBIN in "${pys[@]}"; do
+    echo "Test install $PYBIN $package_name"
     "${PYBIN}/python" -m pip install $package_name --no-index -f /io/wheelhouse
     # Manylinux does not have a JVM so there is no way to test the wheel in the docker
     # "${PYBIN}/pip" install -r /io/test-requirements.txt
