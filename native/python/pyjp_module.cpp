c5132378a4bd935741bf4655428f785593d83d72
Karl Nelson <nelson85@llnl.gov>
2020-10-23 09:59:36 -0700

    Bump version: 1.1.2 → 1.1.3_dev0

--- native/python/pyjp_module.cpp
+++ native/python/pyjp_module.cpp
@@ -712,7 +712,7 @@ PyMODINIT_FUNC PyInit__jpype()
 	// PyJPModule = module;
 	Py_INCREF(module);
 	PyJPModule = module;
-	PyModule_AddStringConstant(module, "__version__", "1.1.2");
+	PyModule_AddStringConstant(module, "__version__", "1.1.3_dev0");
 
 	// Initialize each of the python extension types
 	PyJPClass_initType(module);
