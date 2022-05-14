3b6b052dd2a25ac93e9395e67da1ba54fde52781
Karl E. Nelson <nelson85@llnl.gov>
2022-05-14 06:17:03 -0700

    Trying to fix javadoc on JDK 11

--- setupext/test_java.py
+++ setupext/test_java.py
@@ -87,4 +87,4 @@ class TestJavaCommand(distutils.cmd.Command):
         cmdStr = compileJava()
         self.announce("  %s" % " ".join(cmdStr), level=distutils.log.INFO)
         subprocess.check_call(cmdStr)
-        subprocess.check_call(shlex.split("javadoc test/harness/jpype/doc/Test.java -d test/classes/"))
+        subprocess.check_call(shlex.split("javadoc -Xdoclint:none test/harness/jpype/doc/Test.java -d test/classes/"))
