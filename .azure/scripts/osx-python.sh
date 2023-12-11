dad8cbec2e8ffd1e6b28b0e41e7600d21c428e33
Karl Nelson <nelson85@llnl.gov>
2023-12-10 17:25:17 -0800

    Update dists

--- .azure/scripts/osx-python.sh
+++ .azure/scripts/osx-python.sh
@@ -16,13 +16,16 @@ case $PYTHON_VERSION in
   INSTALLER_NAME=python-$FULL_VERSION-macosx10.9.pkg
   ;;
 3.10)
-  FULL_VERSION=3.10.4
+  FULL_VERSION=3.10.11
   INSTALLER_NAME=python-$FULL_VERSION-macos11.pkg
   ;;
 3.11)
-  FULL_VERSION=3.11.0
+  FULL_VERSION=3.11.7
   INSTALLER_NAME=python-$FULL_VERSION-macos11.pkg
   ;;
+3.12)
+  FULL_VERSION=3.12.0
+  INSTALLER_NAME=python-$FULL_VERSION-macos11.pkg
 esac
 
 URL=https://www.python.org/ftp/python/$FULL_VERSION/$INSTALLER_NAME
