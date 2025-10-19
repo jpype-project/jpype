e2ab98986af07d99a2f425f8313e78066038b64c
Martin K. Scherer <m.scherer@fu-berlin.de>
2025-10-19 19:01:18 +0200

    setup jvm per pytest session

--- test/jpypetest/conftest.py
+++ test/jpypetest/conftest.py
@@ -19,6 +19,7 @@
 import pytest
 
 import common
+import jpype
 
 
 def pytest_addoption(parser):
@@ -45,3 +46,57 @@ def common_opts(request):
     request.cls._convertStrings = request.config.getoption("--convertStrings")
     request.cls._jacoco = request.config.getoption("--jacoco")
     request.cls._checkjni = request.config.getoption("--checkjni")
+
+@pytest.fixture(scope="session")
+def jvm_session(request):
+    import _jpype
+    from os import path
+    import logging
+
+    assert not jpype.isJVMStarted()
+    try:
+        import faulthandler
+        faulthandler.enable()
+        faulthandler.disable()
+    except:
+        pass
+
+
+    _classpath = request.config.getoption("--classpath")
+    _convertStrings = request.config.getoption("--convertStrings")
+    _jacoco = request.config.getoption("--jacoco")
+    _checkjni = request.config.getoption("--checkjni")
+
+    root = path.dirname(path.abspath(path.dirname(__file__)))
+    jpype.addClassPath(path.join(root, 'classes'))
+    jvm_path = jpype.getDefaultJVMPath()
+    logger = logging.getLogger(__name__)
+    logger.info("Running testsuite using JVM %s" % jvm_path)
+    classpath_arg = "-Djava.class.path=%s"
+    args = ["-ea", "-Xmx256M", "-Xms16M"]
+    if _checkjni:
+        args.append("-Xcheck:jni")
+    # TODO: enabling this check crashes the JVM with: FATAL ERROR in native method: Bad global or local ref passed to JNI
+    # "-Xcheck:jni",
+    if _classpath:
+        from pathlib import Path
+        import warnings
+        # This needs to be relative to run location
+        jpype.addClassPath(Path(_classpath).resolve())
+        warnings.warn("using jar instead of thunks")
+    if _convertStrings:
+        import warnings
+        warnings.warn("using deprecated convertStrings")
+    if _jacoco:
+        import warnings
+        args.append(
+            "-javaagent:lib/org.jacoco.agent-0.8.5-runtime.jar=destfile=build/coverage/jacoco.exec,includes=org.jpype.*")
+        warnings.warn("using JaCoCo")
+    jpype.addClassPath(path.join(root, "../lib/*"))
+    jpype.addClassPath(path.join(root, "jar/*"))
+    classpath_arg %= jpype.getClassPath()
+    args.append(classpath_arg)
+    _jpype.enableStacktraces(True)
+    #JPypeTestCase.str_conversion = eval(os.getenv('JPYPE_STR_CONVERSION', 'True'))
+    jpype.startJVM(jvm_path, *args,
+                   convertStrings=_convertStrings)
