1eb167bf40e42147aa9ae7adc229a8b99f0e6d7a
Martin K. Scherer <m.scherer@fu-berlin.de>
2025-10-29 13:25:00 +0100

    cleanup

--- test/jpypetest/common.py
+++ test/jpypetest/common.py
@@ -95,53 +95,7 @@ class UseFunc(object):
 @pytest.mark.usefixtures("jvm_session")
 class JPypeTestCase(unittest.TestCase):
     def setUp(self):
-        if not jpype.isJVMStarted():
-            try:
-                import faulthandler
-                faulthandler.enable()
-                faulthandler.disable()
-            except:
-                pass
-            root = path.dirname(path.abspath(path.dirname(__file__)))
-            jpype.addClassPath(path.join(root, 'classes'))
-            jvm_path = jpype.getDefaultJVMPath()
-            logger = logging.getLogger(__name__)
-            logger.info("Running testsuite using JVM %s" % jvm_path)
-            classpath_arg = "-Djava.class.path=%s"
-            args = ["-ea", "-Xmx256M", "-Xms16M"]
-            if self._checkjni:
-                args.append("-Xcheck:jni")
-            # TODO: enabling this check crashes the JVM with: FATAL ERROR in native method: Bad global or local ref passed to JNI
-            # "-Xcheck:jni",
-            if self._classpath:
-                from pathlib import Path
-                import warnings
-                # This needs to be relative to run location
-                jpype.addClassPath(Path(self._classpath).resolve())
-                warnings.warn("using jar instead of thunks")
-            if self._convertStrings:
-                import warnings
-                warnings.warn("using deprecated convertStrings")
-            if self._jacoco:
-                import warnings
-                args.append(
-                    "-javaagent:lib/org.jacoco.agent-0.8.5-runtime.jar=destfile=build/coverage/jacoco.exec,includes=org.jpype.*")
-                warnings.warn("using JaCoCo")
-            jpype.addClassPath(path.join(root, "../lib/*"))
-            jpype.addClassPath(path.join(root, "jar/*"))
-            classpath_arg %= jpype.getClassPath()
-            args.append(classpath_arg)
-            _jpype.enableStacktraces(True)
-            #JPypeTestCase.str_conversion = eval(os.getenv('JPYPE_STR_CONVERSION', 'True'))
-            jpype.startJVM(jvm_path, *args,
-                           convertStrings=self._convertStrings)
-        else:
-            pass
-            #raise RuntimeError("should be started by fixture (temp)")
         self.jpype = jpype.JPackage('jpype')
-
-    def tearDown(self):
-        pass
 
     def assertElementsEqual(self, a, b):
         self.assertEqual(len(a), len(b))
