b7faaff872180a69941f382365a2fcefdbe5d84a
Martin K. Scherer <m.scherer@fu-berlin.de>
2025-11-14 10:13:56 +0100

    future upgrade

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ autosectionlabel_prefix_document = True
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = {'.rst': 'restructuredtext'}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
